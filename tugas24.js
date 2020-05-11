1133c846b9c8bb2d24d4b0855561a6fd98abe80e
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:42:12 +0700

    Tugas 24 JS
    Buatlah sorting dan reverse dari deret angka array ini : 32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66

---
+++ tugas24.js
@@ -0,0 +1,8 @@
+  let deretAngka = [32,33,16,40,9,7,11,2,44,1,66];
+  console.log("Sebelumnya :",deretAngka.toString());
+
+  let menaik = deretAngka.sort();
+  console.log("Ascending :",menaik.toString());
+
+  let menurun = deretAngka.reverse();
+  console.log("Descending :",menurun.toString());
